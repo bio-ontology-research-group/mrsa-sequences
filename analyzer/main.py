6b240d6b1da6600d83c6c1d5f99c669b06022a2f
Maxat Kulmanov <coolmaksat@gmail.com>
2020-07-13 18:03:50 +0300

    .

--- analyzer/main.py
+++ analyzer/main.py
@@ -9,62 +9,66 @@ import getpass
 import json
 import yaml
 import socket
+import subprocess
+import tempfile
+import logging
 
 
 ARVADOS_API_HOST = os.environ.get('ARVADOS_API_HOST', 'cborg.cbrc.kaust.edu.sa')
 ARVADOS_API_TOKEN = os.environ.get('ARVADOS_API_TOKEN', '')
 
-def upload_file(col, filename_local, filename_remote):
-    lf = open(filename_local, 'rb')
-    with col.open(filename_remote, "wb") as f:
-        r = lf.read(65536)
-        while r:
-            f.write(r)
-            r = lf.read(65536)
-    lf.close()
+def run_workflow(api, parent_project, workflow_uuid, name, inputobj):
+    project = api.groups().create(body={
+        "group_class": "project",
+        "name": name,
+        "owner_uuid": parent_project,
+    }, ensure_unique_name=True).execute()
 
-def validate_fastq(fastq_file):
-    with gzip.open(fastq_file, 'rt') as f:
-        for record in SeqIO.parse(f, 'fastq'):
-            pass
-    return True
+    with tempfile.NamedTemporaryFile() as tmp:
+        tmp.write(json.dumps(inputobj, indent=2).encode('utf-8'))
+        tmp.flush()
+        cmd = ["arvados-cwl-runner",
+               "--submit",
+               "--no-wait",
+               "--project-uuid=%s" % project["uuid"],
+               "arvwf:%s" % workflow_uuid,
+               tmp.name]
+        logging.info("Running %s" % ' '.join(cmd))
+        comp = subprocess.run(cmd, capture_output=True)
+    if comp.returncode != 0:
+        logging.error(comp.stderr.decode('utf-8'))
 
+    return project
 
 @ck.command()
 @ck.option('--fastq-project', '-fp', default='cborg-j7d0g-y651nepk74ziw3p', help='MRSA FASTQ sequences project uuid')
-@ck.option('--metagenome-workflow-uuid', '-mwid', default='', help='Metagenome workflow uuid')
+@ck.option('--workflows-project', '-wp', default='cborg-j7d0g-lcux1tdrdshvul7', help='MRSA workflows project uuid')
+@ck.option('--metagenome-workflow-uuid', '-mwid', default='cborg-7fd4e-3ig4fl4bz90uydt', help='Metagenome workflow uuid')
 @ck.option('--pangenome-workflow-uuid', '-pwid', default='', help='Pangenome workflow uuid')
-def main(fastq_project, metagenome_workflow_uuid, pangenome_workflow_uuid):
-    metadata = yaml.load(open(metadata_file), Loader=yaml.FullLoader)
-    validate_fastq(sequence_read1)
-    validate_fastq(sequence_read2)
-    
+def main(fastq_project, workflows_project, metagenome_workflow_uuid, pangenome_workflow_uuid):    
     api = arvados.api('v1', host=ARVADOS_API_HOST, token=ARVADOS_API_TOKEN)
     col = arvados.collection.Collection(api_client=api)
 
-    upload_file(col, sequence_read1, 'reads1.fastq.gz')
-    upload_file(col, sequence_read2, 'reads2.fastq.gz')
-    upload_file(col, metadata_file, 'metadata.yaml')
-    external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+    reads = arvados.util.list_all(api.collections().list, filters=[["owner_uuid", "=", fastq_project]])
+    for r in reads[:1]:
+        inputobj = {}
+        inputobj["fastq1"] = {
+            "class": "File",
+            "location": "keep:%s/reads1.fastq.gz" % r["portable_data_hash"]
+        }
+        inputobj["fastq2"] = {
+            "class": "File",
+            "location": "keep:%s/reads2.fastq.gz" % r["portable_data_hash"]
+        }
+        inputobj["metadata"] = {
+            "class": "File",
+            "location": "keep:%s/metadata.yaml" % r["portable_data_hash"]
+        }
+        sample_id = r['properties']['sequence_label']
+        name = f'Metagenome analysis for {sample_id}'
+        run_workflow(api, workflows_project, metagenome_workflow_uuid, name, inputobj)
 
-    try:
-        username = getpass.getuser()
-    except KeyError:
-        username = "unknown"
-
-    properties = {
-        "sequence_label": metadata['sample'],
-        "upload_app": "mrsa_uploader",
-        "upload_ip": external_ip,
-        "upload_user": "%s@%s" % (username, socket.gethostname())
-    }
-
-    col.save_new(owner_uuid=uploader_project, name="%s uploaded by %s from %s" %
-                 (metadata['sample'], properties['upload_user'], properties['upload_ip']),
-                 properties=properties, ensure_unique_name=True)
-
-    print(json.dumps(col.api_response()))
-
+    
 
 if __name__ == '__main__':
     main()
---
+++ workflows/update-workflows.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+arvados-cwl-runner --project-uuid=cborg-j7d0g-lcux1tdrdshvul7 --update-workflow=cborg-7fd4e-3ig4fl4bz90uydt mrsa-metagenome.cwl
