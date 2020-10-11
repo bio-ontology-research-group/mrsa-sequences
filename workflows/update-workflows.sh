8e927f6be5fbd24dd9d0d7898fba6d255a2b89f4
Maxat Kulmanov <coolmaksat@gmail.com>
2020-10-11 10:03:38 +0300

    .

--- workflows/update-workflows.sh
+++ workflows/update-workflows.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-arvados-cwl-runner --project-uuid=cborg-j7d0g-lcux1tdrdshvul7 --update-workflow=cborg-7fd4e-3ig4fl4bz90uydt mrsa-metagenome.cwl
-arvados-cwl-runner --project-uuid=cborg-j7d0g-lcux1tdrdshvul7 --update-workflow=cborg-7fd4e-qhxoc5ddgrti3tq mrsa-pangenome.cwl
+arvados-cwl-runner --project-uuid=cborg-j7d0g-lcux1tdrdshvul7 --update-workflow=cborg-7fd4e-3ig4fl4bz90uydt metagenome/mrsa-metagenome.cwl
+arvados-cwl-runner --project-uuid=cborg-j7d0g-lcux1tdrdshvul7 --update-workflow=cborg-7fd4e-qhxoc5ddgrti3tq pangenome/mrsa-pangenome.cwl
